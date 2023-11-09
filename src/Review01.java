7eb17869ba5267a1f96a726399ccdb956160c90c
nobil <nobil@192.168.10.28>
2023-11-09 23:52:13 +0900

    Lesson3課題で作成

---
+++ src/Review01.java
@@ -0,0 +1,17 @@
+
+public class Review01 {
+
+    public static void main(String[] args) {
+        int price = 1500;
+        int tax;
+        tax = taxMethod(price);
+        int taxInPrice = price + tax;
+        System.out.println(price + "円の商品の税込価格は" + taxInPrice + "円（消費税は" + tax + "円）です。");
+    }
+
+    public static int taxMethod(int price) {
+        int per = 10;
+        int tax = price * per / 100;
+        return tax;
+    }
+}
