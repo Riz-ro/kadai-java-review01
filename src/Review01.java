1975f34d754e1b143cff9d8094784536e594bdd8
nobil <nobil@192.168.10.28>
2023-11-10 15:43:49 +0900

    taxメソッド名を修正

--- src/Review01.java
+++ src/Review01.java
@@ -4,12 +4,12 @@ public class Review01 {
     public static void main(String[] args) {
         int price = 1500;
         int tax;
-        tax = taxMethod(price);
+        tax = tax(price);
         int taxInPrice = price + tax;
         System.out.println(price + "円の商品の税込価格は" + taxInPrice + "円（消費税は" + tax + "円）です。");
     }
 
-    public static int taxMethod(int price) {
+    public static int tax(int price) {
         int per = 10;
         int tax = price * per / 100;
         return tax;
